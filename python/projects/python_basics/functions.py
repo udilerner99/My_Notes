c2414607203a65e004e5b0879c1cc781b42878ee
ulerner <ulerner@ebay.com>
2022-02-01 14:29:19 +0200

    loops and functions

---
+++ python/projects/python_basics/functions.py
@@ -0,0 +1,21 @@
+# A function is a block of code which only runs when it is called.
+#
+# You can pass data, known as parameters, into a function.
+#
+# A function can return data as a result.
+#
+# Creating a Function
+# In Python a function is defined using the def keyword:
+#
+# Example
+def my_function():
+  print("Hello from a function")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis:
+
+# Example
+def my_function():
+  print("Hello from a function")
+
+my_function()
