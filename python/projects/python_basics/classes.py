f9af266f632046752a189c3417fcf97b0854ab5e
ulerner <ulerner@ebay.com>
2022-02-02 11:50:53 +0200

    classes

---
+++ python/projects/python_basics/classes.py
@@ -0,0 +1,46 @@
+# Python Classes/Objects
+# Python is an object oriented programming language.
+#
+# Almost everything in Python is an object, with its properties and methods.
+#
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+#
+# Create a Class
+# To create a class, use the keyword class:
+#
+# Example
+# Create a class named MyClass, with a property named x:
+
+class MyClass:
+  x = 5
+
+# Create Object
+# Now we can use the class named MyClass to create objects:
+#
+# Example
+# Create an object named p1, and print the value of x:
+
+p1 = MyClass()
+print(p1.x)
+
+# The __init__() Function
+# The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+#
+# To understand the meaning of classes we have to understand the built-in __init__() function.
+#
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+#
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+#
+# Example
+# Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
