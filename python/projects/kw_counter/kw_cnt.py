34a428a3998798fe5e7e47e88dbec7b4c94293da
ulerner <ulerner@ebay.com>
2022-02-08 11:16:44 +0200

    key word counter

---
+++ python/projects/kw_counter/kw_cnt.py
@@ -0,0 +1,10 @@
+# """
+# This program will count key word in the given text from a file
+# seperator is a white space
+# """
+
+f = open("demofile.txt", "r")
+for x in f:
+    print("current line is: {}".format(x))
+    print("number of words in line is: ".format(x.split(" ")))
+    print(len(x.split(" ")))
