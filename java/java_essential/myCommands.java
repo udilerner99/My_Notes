26c7a733bd4f4b36ff0ffc3998fd3ac49814f9c2
ulerner <ulerner@ebay.com>
2022-02-17 16:47:53 +0200

    java essential

---
+++ java/java_essential/myCommands.java
@@ -0,0 +1,5 @@
+String s1 = "hello";
+s1
+var i1 = 5;
+var i2 = 10;
+var result = i1+i2;
---
+++ java/java_essential/src/com/example/idea/Main.java
@@ -0,0 +1,9 @@
+package com.example.idea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Hello" + args[0] + " !");
+    }
+}
