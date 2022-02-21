04dd99ae1ca10fa6fc8b73ca2e208034173e0f3c
ulerner <ulerner@ebay.com>
2022-02-21 16:42:40 +0200

    java essential

---
+++ java/java_essential/builder.java
@@ -0,0 +1,6 @@
+var sb = new StringBuilder("welcome");
+sb.append(" to california");
+var s = sb.toString();
+StringBuilder b = new StringBuilder();
+b.append("Shirt size: ").append("M").append(", Qty: ").append(4);
+var s2 = b.toString();
