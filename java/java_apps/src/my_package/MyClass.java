74a0f5187bdba59cfc2636f47673af0d4e439117
ulerner <ulerner@ebay.com>
2022-02-28 12:24:17 +0200

    classes

---
+++ java/java_apps/src/my_package/MyClass.java
@@ -0,0 +1,14 @@
+package my_package;
+
+public class MyClass {
+
+	public int myInt = 5;
+
+	public MyClass() {
+		System.out.println("Constructor");
+	}
+
+	void myMethod() {
+		System.out.println("my method");
+	}
+}
--- java/java_apps/src/my_package/Review.java
+++ java/java_apps/src/my_package/Review.java
@@ -3,15 +3,12 @@ package my_package;
 public class Review {
 
 	public static void main(String[] args) {
-		boolean statment1 = 1 == 1;
-		boolean statment2 = 1 > 3;
 
-		if (statment1 && statment2) {
-			System.out.println("Statement is true");
-		} else {
-			System.out.println("Statement is false");
-		}
-
+		MyClass mc = new MyClass();
+		mc.myMethod();
+		System.out.println(mc.myInt);
+		mc.myInt = 10;
+		System.out.println(mc.myInt);
 	}
 
 }
