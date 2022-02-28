eb231b0b032c8811ccf7250618aa26e884781ecd
ulerner <ulerner@ebay.com>
2022-02-28 13:01:00 +0200

    handle errors

--- java/java_apps/src/my_package/Review.java
+++ java/java_apps/src/my_package/Review.java
@@ -9,7 +9,15 @@ import java.util.List;
 public class Review {
 
 	public static void main(String[] args) throws IOException {
-		Path file = FileSystems.getDefault().getPath(System.getProperty("user.dir") + "/src/", "file.txt");
+		try {
+			loadFile("file.txt");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	static void loadFile(String fileName) throws IOException {
+		Path file = FileSystems.getDefault().getPath(System.getProperty("user.dir") + "/src/", fileName);
 		List<String> lines = Files.readAllLines(file);
 		//for (int i = 0; i < lines.size(); i++) {
 		//	System.out.println(lines.get(i));
