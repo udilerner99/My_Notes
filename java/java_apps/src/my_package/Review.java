48bcbdd3c55c9cc7cf61be408549ffc12090b9c2
ulerner <ulerner@ebay.com>
2022-02-28 12:53:57 +0200

    read from file

--- java/java_apps/src/my_package/Review.java
+++ java/java_apps/src/my_package/Review.java
@@ -1,15 +1,21 @@
 package my_package;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Review {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter your name: ");
-		String name = scanner.nextLine();
-		System.out.println("Your name is: " + name);
-		scanner.close();
+	public static void main(String[] args) throws IOException {
+		Path file = FileSystems.getDefault().getPath(System.getProperty("user.dir") + "/src/", "file.txt");
+		List<String> lines = Files.readAllLines(file);
+		//for (int i = 0; i < lines.size(); i++) {
+		//	System.out.println(lines.get(i));
+		//}
+		//lines.forEach((str) ->System.out.println(str));
+		lines.forEach(System.out::println);
 	}
 
 }
